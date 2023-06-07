3b922ee2237e2bf0275ced70d45018aa7219cbcb
reddevill007 <shivampandey2900@gmail.com>
2023-06-07 08:02:45 +0530

    init

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <p>Lorem ipsum dolor sit, placeat.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
